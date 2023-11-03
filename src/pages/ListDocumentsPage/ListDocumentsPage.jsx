becce960283ace1a527b60b97389fb2e3e80668b
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2023-11-03 18:20:27 +0100

    fix48

--- src/pages/ListDocumentsPage/ListDocumentsPage.jsx
+++ src/pages/ListDocumentsPage/ListDocumentsPage.jsx
@@ -12,12 +12,12 @@ const ListDocumentsPage = () => {
 
   const getDocs = async () => {
     try {
-      console.log(`Bearer ${String(authToken?.access)}`)
+      console.log(`Bearer ${authToken?.access}`)
       let response = await fetch("http://apisindikat.skupstina.me/api/important/", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${String(authToken?.access)}`,
+          Authorization: `Bearer ${authToken?.access}`,
         },
       });
 
