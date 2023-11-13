d186aa1db965de950c2da805192f39476e18b20b
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2023-11-13 20:11:44 +0100

    353

--- src/pages/StaffSinglePage/StaffSinglePage.jsx
+++ src/pages/StaffSinglePage/StaffSinglePage.jsx
@@ -145,7 +145,7 @@ const StaffSinglePage = () => {
           {
             staff?.active_role && staff?.active_role[0] !== "Bivši predsjednik" && (
               <div className="StaffSinglePage__card-info__container">
-                <p>Tel: <a>{staff?.phone}</a></p>
+                {/* <p>Tel: <a>{staff?.phone}</a></p> */}
                 <p>Email: <a>{staff?.email}</a></p>
               </div>
             )
