e4437c587a9d7ae56a7a5d66dffb6e28c79fbb24
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2023-11-22 22:44:45 +0100

    fix-508

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -84,13 +84,13 @@ const HamburgerMenu = () => {
 
     return (
       <div className={"HamburgerMenu__container transparent"}>
-        <div className="HamburgerMenu__img-container" onClick={handleisMenuOpen}>
+        <div className="HamburgerMenu__img-container" onClick={()=> {handleisMenuOpen}}>
           <img src={!isMenuOpen ? openMenu : closeMenu} alt="" />
         </div>
         <div className={isMenuOpen ? "HamburgerMenu__item-container" : "HamburgerMenu__item-container no_content"}>
           {
             menuItems.map((element, index)=> ( 
-              <HamburgerMenuItem onClick={handleisMenuOpen} path={element.path} title={element.title} items={element.items} key={index} />
+              <HamburgerMenuItem onClick={() => {handleisMenuOpen}} path={element.path} title={element.title} items={element.items} key={index} />
             ))
           } 
         
