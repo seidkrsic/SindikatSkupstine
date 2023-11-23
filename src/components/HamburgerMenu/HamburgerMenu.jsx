fbf7a1bcad27aaaf4be205ca5efb05fcb2da1dc5
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2023-11-23 01:20:58 +0100

    fix-567

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -92,6 +92,7 @@ const HamburgerMenu = () => {
         <div className={isMenuOpen ? "HamburgerMenu__item-container" : "HamburgerMenu__item-container no_content"}>
           {
             menuItems.map((element, index)=> ( 
+              
               <HamburgerMenuItem onClick={handleisMenuOpen} path={element.path} title={element.title} items={element.items} key={index} />
             ))
           } 
