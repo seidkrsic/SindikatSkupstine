7c3ee2fc5b1d7d587592cf146403963e65a7495a
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2023-11-26 10:05:12 +0100

    fix-629

--- src/pages/StaffSinglePage/StaffSinglePage.jsx
+++ src/pages/StaffSinglePage/StaffSinglePage.jsx
@@ -6,7 +6,7 @@ import StaffCard from "../../components/StaffCard/StaffCard";
 import AuthContext from "../../Context/AuthContext";
 import "../StaffSinglePage/StaffSinglePage.css";
 import parse from "html-react-parser";
-import salon from "../../images/plavi_salon.jpg"
+import salon from "../../images/zeleni_salon.jpg"
 
 const StaffSinglePage = () => {
   let CategoriesInfo;
